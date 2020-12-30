2babfd83453ca71247b7d9def09c1f6281d1f728
leducdo-lab <61894722+leducdo-lab@users.noreply.github.com>
2020-12-30 16:50:20 +0700

    Add files via upload

---
+++ src/Models/Products.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace TDP\User\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Products extends Model
+{
+    protected $table = 'product';
+    protected $primaryKey = 'id';
+
+    public function Carts() {
+        return $this->hasMany('TDP\User\Models\Carts', 'product_id', 'local_key');
+    }
+
+    public function Images() {
+        return $this->hasMany('TDP\User\Models\Images', 'product_id', 'local_key');
+    }
+
+    public function PK_Product() {
+        return $this->hasMany('TDP\User\Models\PK_Product', 'product_id', 'local_key');
+    }
+}
+
